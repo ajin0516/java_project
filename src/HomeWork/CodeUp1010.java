3cb8690fd25a3f5e54dbb26d9fd74d221f118a52
ajin0516 <dkwls7432@naver.com>
2022-10-04 23:46:45 +0900

    CodeUP 과제

---
+++ src/HomeWork/CodeUp1010.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class CodeUp1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("입력한 숫자 " + num);
+    }
+}
