e08906ea04d97c42d25a155cf45969682da02667
ajin0516 <dkwls7432@naver.com>
2022-10-05 10:24:31 +0900

    Calculator 계산

---
+++ src/Java1005/CacculatorMain.java
@@ -0,0 +1,15 @@
+package Java1005;
+
+public class CacculatorMain {
+    public static void main(String[] args) {
+        Cacculator cal = new Cacculator();
+        int a = 6;
+        int b = 3;
+
+        cal.plus(a,b);
+        cal.minus(a,b);
+        cal.multiple(a,b);
+        cal.divide(a,b);
+
+    }
+}
