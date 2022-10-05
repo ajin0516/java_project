6e5e738e7076bbf266c69264268528c698da38de
ajin0516 <dkwls7432@naver.com>
2022-10-06 02:00:33 +0900

    10/5일 인터페이스 실습

--- src/Java1005/collection/LikeLion2th.java
+++ src/Java1005/collection/LikeLion2th.java
@@ -5,6 +5,7 @@ import java.util.List;
 // Shift + F6 = 동시에 수정할 때(커서여러개)
 public class LikeLion2th {
     // 멋사 2기 학생의 이름이 들어있는 List를 return하는 method
+
     private List<String> students = new ArrayList<>();
 
     Names names = new Names();
---
+++ src/Java1005/collection/Student.java
@@ -0,0 +1,15 @@
+package Java1005.collection;
+
+public class Student {
+    private  int classNo;
+    private String name;
+    private String gitRepositoryAddress;
+
+    public Student(int classNo, String name, String gitRepositoryAddress) {
+        this.classNo = classNo;
+        this.name = name;
+        this.gitRepositoryAddress = gitRepositoryAddress;
+    }
+
+
+}
