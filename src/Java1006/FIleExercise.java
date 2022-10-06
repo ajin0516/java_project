51854429bf93ffd11fca62a5d61ee5dd4e6ed5ee
ajin0516 <dkwls7432@naver.com>
2022-10-06 15:40:17 +0900

    File에서 한글자, 두글자 읽어오기

--- src/Java1006/FIleExercise.java
+++ src/Java1006/FIleExercise.java
@@ -2,23 +2,39 @@ package Java1006;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FIleExercise {
-    public static void main(String[] args) throws FileNotFoundException {
-
+    public void printFiles(){
         File dir = new File("./");
         File files[] = dir.listFiles();
+
         for (File file : files) {
             System.out.println(file);
         }
     }
+    // 한글자 읽어오기
+    public char readAChar(String filename) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        return (char)fileReader.read();
+    }
+    // 두글자 읽어오기
+    public String read2Chars(String filename) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str += (char)fileReader.read();
+        str += (char)fileReader.read();
+        return str;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // ./
+        FIleExercise fIleExercise = new FIleExercise();
+        char ch = fIleExercise.readAChar("aa_file.txt");
+        String str = fIleExercise.read2Chars("aa_file.txt");
+        System.out.println(ch);
+        System.out.println(str);
+    }
+
 }
-/*
-.\.git
-.\.gitignore
-.\.idea
-.\a_file.txt
-.\LikeLion.iml
-.\out
-.\src
- */
