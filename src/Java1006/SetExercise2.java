cc813c44547f6f67bf329329124e8eaf17f11907
ajin0516 <dkwls7432@naver.com>
2022-10-06 10:53:07 +0900

    Set - Alphabet 50개 랜덤 생성 후 중복제거

---
+++ src/Java1006/SetExercise2.java
@@ -0,0 +1,16 @@
+package Java1006;
+
+import java.util.HashSet;
+
+public class SetExercise2 {
+    public static void main(String[] args) {
+        AlphabetGenerator alphabetGenerator = new AlphabetGenerator();
+        HashSet<Character> hashSet = new HashSet<>();
+
+        for(int i=0; i<50; i++) {
+            char alphabet = alphabetGenerator.create();
+            hashSet.add(alphabet);
+        }
+        System.out.println(hashSet);
+    }
+}
