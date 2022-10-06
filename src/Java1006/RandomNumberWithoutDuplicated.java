10c82b4cec1ce5fb341bebe8b6091373e485daa3
ajin0516 <dkwls7432@naver.com>
2022-10-06 10:16:21 +0900

    random number 50개를 생성

--- src/Java1006/RandomNumberWithoutDuplicated.java
+++ src/Java1006/RandomNumberWithoutDuplicated.java
@@ -2,9 +2,10 @@ package Java1006;
 
 public class RandomNumberWithoutDuplicated {
     public static void main(String[] args) {
-        RandomNumberGenerator RandomNumberGenerator = new RandomNumberGenerator();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
         for(int i=0; i<50; i++){
-            int r = RandomNumberGenerator.generate(10);
+            int r = randomNumberGenerator.generate(10);
             System.out.println(r);
         }
     }
