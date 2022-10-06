27c9aae8e9c325de1d3b543dfc3373da4c273313
ajin0516 <dkwls7432@naver.com>
2022-10-06 10:32:02 +0900

    random number 50개를 생성 후 중복제거

--- src/Java1006/RandomNumberGenerator.java
+++ src/Java1006/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ public class RandomNumberGenerator implements NumberGenerator{
 
     @Override
     public int generate(int num) {
-        return num;
+        return (int)(Math.random()*num);
     }
 }
 
--- src/Java1006/RandomNumberWithoutDuplicated.java
+++ src/Java1006/RandomNumberWithoutDuplicated.java
@@ -1,12 +1,18 @@
 package Java1006;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RandomNumberWithoutDuplicated {
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
+        HashSet<Integer> numbers = new HashSet<>(); // Set - 중복제거
+
         for(int i=0; i<50; i++){
-            int r = randomNumberGenerator.generate(10);
-            System.out.println(r);
+            numbers.add(randomNumberGenerator.generate(30));
         }
+        System.out.println(numbers);
+        System.out.println("총 " + numbers.size() + "개 입니다.");
     }
 }
