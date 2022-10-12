5cc3abd3b11d480ce3de23b27d713d7257fca87e
ajin0516 <dkwls7432@naver.com>
2022-10-12 23:49:45 +0900

    CodeUp 1051~1060

---
+++ src/HomeWork/CodeUp1060.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class CodeUp1060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((a&b));
+
+    }
+}
+
+/*
+~n = -n-1
+-n = ~n+1
+ */
