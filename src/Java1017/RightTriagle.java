a88fc88bc40de7545e03f4cbd800c54e4e6dc873
ajin0516 <dkwls7432@naver.com>
2022-10-17 09:36:22 +0900

    #,@ 찍기

--- src/Java1017/RightTriagle.java
+++ src/Java1017/RightTriagle.java
@@ -1,9 +1,19 @@
 package Java1017;
 public class RightTriagle {
+    private String letter = "#";
+
+    // 기본값일 때
+    public RightTriagle() {
+    }
+    // 파라미터 있을 기
+    public RightTriagle(String letter) {
+        this.letter = letter;
+    }
+
     public void starNum(int num){
         for(int i = 0; i < num; i++){
             for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(this.letter);
             }
             System.out.println();
         }
