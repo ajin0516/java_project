7a7baeff8aeaecaf72cc77c22ff43705955129a7
ajin0516 <dkwls7432@naver.com>
2022-10-28 10:02:13 +0900

    [프로그래머스] 포켓몬 설계 다시 하기

--- src/programmers/pokemon.java
+++ src/programmers/pokemon.java
@@ -10,15 +10,22 @@ import java.util.Set;
 
 public class pokemon {
         public int solution(int[] nums) {
-            Set<Integer> set = new HashSet<>(); // 중복제거 위해 set사용
+            // 같은 번호는 지우고 다른 번호들만 남기기?!
+            Set<Integer> set = new HashSet<>(); // 중복제거 위해 set사용 - [3,3,3,2,2,2] -> [3,2]
             for (int i = 0; i < nums.length; i++) {
                 set.add(nums[i]);
             }
 
-            // 중복을 없애기 위해서?
+            // 중복을 없애기 위해서??
             int N = nums.length /2 ;
 
-            // 같은 번호는 지우고 다른 번호들만 남기는 건?..
+            // N의 개수보다 set개수가 적으면 중복이 제거 된거라서 set개수 리턴
+            // 그렇지 않다면 동일하거나 많은거라 정해진 개수 N만큼만 리턴
+            if(set.size() < N){
+                return set.size();
+            }else {
+                return N;
+            }
 
         }
 
