755026f10ab70457b659ae83ef166fc208b25b9a
ajin0516 <dkwls7432@naver.com>
2022-11-01 10:46:33 +0900

    [프로그래머스] 소수 구하기 TemplateCallback 내북클래스, 람다식 적용

--- src/programmers/TemplateCallback.java
+++ src/programmers/TemplateCallback.java
@@ -1,16 +1,20 @@
 package programmers;
 
-public class TemplateCallback {
-    boolean someOperation(int a, int b){
-        return a < b;
-    }
+interface StatementStrategy{
+    boolean compare(int a, int b);
+}
 
-    boolean isPrime(int num) {
+public class TemplateCallback {
+//    boolean someOperation(int a, int b){
+//        return a < b;
+//    }
+
+    boolean isPrime(int num, StatementStrategy stmt) {
         // i < num
         // i < num / 2
         // i * i < num
-
-        for (int i = 2; someOperation(1, num); i++) {
+        for (int i = 2; stmt.compare(i, num); i++) {
+            System.out.println(i);
             if (num % i == 0) return false;
         }
         return true;
@@ -19,7 +23,17 @@ public class TemplateCallback {
 
     public static void main(String[] args) {
         TemplateCallback templateCallback = new TemplateCallback();
-        boolean r = templateCallback.isPrime(10);
-        System.out.println(r);
+        // 내부클래스
+//        boolean r = templateCallback.isPrime(17, new StatementStrategy() {
+//            @Override
+//            public boolean compare(int a, int b) {
+//                return a * a <= b;
+//            }
+//        });
+
+        // 람다식 적용
+        System.out.println(templateCallback.isPrime(13, (a, b)-> a < b));
+        System.out.println(templateCallback.isPrime(17, (a, b)-> a < b/2));
+        System.out.println(templateCallback.isPrime(19, (a, b)-> a * a < b));
     }
 }
