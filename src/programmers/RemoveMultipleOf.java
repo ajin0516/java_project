617bd70fdcc2360092dd1be9c535f6a1b7e2d3e4
ajin0516 <dkwls7432@naver.com>
2022-11-02 09:52:54 +0900

    [프로그래머스] 소수 구하기 에라토스테네스의 체 적용

---
+++ src/programmers/RemoveMultipleOf.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveMultipleOf {
+
+    public static void main(String[] args) {
+        int N = 50; // 50미만의 모든 소수 구하기
+
+        List<Integer> nums = new ArrayList<>();
+
+        // 2~50채우기
+        for (int i = 2; i <= N; i++) {
+            nums.add(i);
+            System.out.println(nums);
+        }
+
+        // 2의 배수 지우기 2제외
+        // 1부터 시작하면 ?
+        for (int i = 1; i < nums.size(); i++) {
+            if(nums.get(i) % 2 == 0){
+                nums.remove(i);
+            }
+        }
+        System.out.println(nums);
+//        nums.removeIf( num -> num % 2 == 0 && num !=2);
+    }
+}
