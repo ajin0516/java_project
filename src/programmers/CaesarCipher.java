252ac66fa536cba2823e1f3c1ae22908c24504bb
ajin0516 <dkwls7432@naver.com>
2022-11-18 10:03:19 +0900

    [프로그래머스] 시저 암호 테스트 실패

--- src/programmers/CaesarCipher.java
+++ src/programmers/CaesarCipher.java
@@ -3,7 +3,8 @@ package programmers;
 public class CaesarCipher {
 
     public String solution(String s, int n) {
-        String answer = "z";
+        String answer = "Z";
+        int num = 4;
         int a = 97; // a
         int b = 122; // z
         int c = 65; // A
@@ -17,12 +18,18 @@ public class CaesarCipher {
             int x = (int)answer.charAt(i) + 0;
 
             if(x >= 97 && x <= 122){
-                int plusOne = x + 1;
+                int plusOne = x + 1; //123
+                if(plusOne > 122){
+                    plusOne = x - 25;
+                }
                 char plusAlpha = (char)plusOne;
                 result += plusAlpha;
                 System.out.println(result + "1");
             } else if (x >= 65 && x <= 90) {
-                int plusOne = x + 1;
+                int plusOne = x + 1; //91
+                if(plusOne > 90){
+                    plusOne = x -25;
+                }
                 char plusAlpha = (char)plusOne;
                 result += plusAlpha;
                 System.out.println(result + "2");
