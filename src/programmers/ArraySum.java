825eb49076cc55d2e62fcb9a615a12d5f3c1c12e
ajin0516 <dkwls7432@naver.com>
2022-11-28 10:15:04 +0900

    [재귀] AA~ZZ 출력

---
+++ src/programmers/ArraySum.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class ArraySum {
+    static int sum(int[] arr, int answer, int idx) {
+
+        // idx + 1을 해서 탈출조건에 갈수 있게 처리한다
+        if(arr.length == idx){
+            System.out.println("answer = " + answer);
+            return answer;
+        }
+//        System.out.println("arr = " + arr);
+        System.out.println("idx =" + idx);
+        System.out.println("arr[idx] = " + arr[idx]);
+        System.out.println();
+        return sum(arr, answer + arr[idx], idx + 1);
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 3, 2, 9};
+        // 처음 호출 할 때도 0을 추가로 넣어주어서 복잡
+        // parameter를 하나 더 추가 해서 복잡
+        int r =sum(arr, 0,0);
+        System.out.println(r);
+    }
+
+}
